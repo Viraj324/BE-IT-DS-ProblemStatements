497dd569d4d865515663ba28194e4e9d2d100123
Viraj <viraj.sonawane22@pccoepune.org>
2025-04-27 13:42:02 +0530

    Steps add

--- PS1_RMI_ADD/ServerIntf.java
+++ PS1_RMI_ADD/ServerIntf.java
@@ -4,3 +4,21 @@ import java.rmi.*;
 interface ServerIntf extends Remote {
     public int add(int n1,int n2) throws RemoteException;
 }
+
+
+
+//Steps to run 
+/*
+ * 1) Open terminal 1
+ * run "javac *.java"
+ * then run "rmiresgistry"
+ * 
+ * 2) Open terminal 2 
+ * run "java server"
+ * 
+ * 3) Open terminal 3 
+ * run "java client"
+ * enter the input 
+ * ans the ouptut will be displayed 
+ *
+ */
